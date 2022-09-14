4a62cd9e3b5c4c8d5331d468193c18c9d9366662
ErTavo <gusjr11@gmail.com>
2022-09-13 18:50:44 -0600

    correo inicio

--- Proyecto BD/Controllers/HomeController.cs
+++ Proyecto BD/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Proyecto_BD.Controllers
@@ -40,6 +42,33 @@ namespace Proyecto_BD.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        //Boton Reclamar boletos
+
+        private void SendEmail(string EmailDestino, int idboleto)
+        {
+
+
+
+
+            string EmailOrigen = "correo@gmail.com";
+            string Contrase単a = "Hola1234";
+            MailMessage oMailMessage = new MailMessage(EmailOrigen, EmailDestino, "Boletos",
+                "<p>Adjuntamos los boletos para su evento</p><br>");
+
+            oMailMessage.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(EmailOrigen, Contrase単a),
+                Timeout = 30000
+            };
+            smtp.Send(oMailMessage);
+            smtp.Dispose();
         }
     }
 }
